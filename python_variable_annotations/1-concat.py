b19cb17b34e2260d4b0692c02eee195d7c753aa3
StilianSaka <stilisaka@gmail.com>
2024-12-08 21:35:49 +0000

    variable annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module: 1-concat
+
+This module provides a function `concat`
+to concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated result of the two strings.
+    """
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute
+the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the float n.
+
+    Args:
+        n (float): A floating point number
+        to be floored.
+
+    Returns:
+        int: The largest integer less
+        than or equal to n.
+    """
+    return int(n // 1)
